4485fc9b7cf715800a84906c501f949cf6caaf49
RealLord24 <eshunlord699@gmail.com>
2026-03-07 13:10:49 +0000

    Add question bank for programming, DBMS, and OS

---
+++ questions.js
@@ -0,0 +1,59 @@
+const questionBank = {
+
+programming: [
+
+{
+question: "What is computer programming?",
+options: [
+"Using computer hardware",
+"Writing instructions for a computer",
+"Playing games",
+"Repairing computers"
+],
+answer: 1
+},
+
+{
+question: "A step-by-step solution to a problem is called:",
+options: [
+"Program",
+"Algorithm",
+"Syntax",
+"Code"
+],
+answer: 1
+}
+
+],
+
+dbms: [
+
+{
+question: "What does DBMS stand for?",
+options: [
+"Database Management System",
+"Data Binary Model",
+"Data Backup Model",
+"Digital Base System"
+],
+answer: 0
+}
+
+],
+
+os: [
+
+{
+question: "Which software manages computer resources?",
+options: [
+"Browser",
+"Operating System",
+"Compiler",
+"Editor"
+],
+answer: 1
+}
+
+]
+
+};
