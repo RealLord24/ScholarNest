6d023ec38b4762e0c9536db7af53a41eac23b8ec
RealLord24 <eshunlord699@gmail.com>
2026-03-15 17:20:52 +0000

    Update questions.js

--- questions.js
+++ questions.js
@@ -57,3 +57,123 @@ answer: 1
 ]
 
 };
+const questionBank = {
+
+programming: [
+
+{
+question: "What is computer programming?",
+options: [
+"Using computer hardware",
+"Writing instructions for a computer",
+"Playing games on computer",
+"Repairing computer"
+],
+answer: 1
+},
+
+{
+question: "A step-by-step procedure to solve a problem is called:",
+options: [
+"Program",
+"Algorithm",
+"Compiler",
+"Syntax"
+],
+answer: 1
+},
+
+{
+question: "Who writes computer programs?",
+options: [
+"User",
+"Programmer",
+"Operator",
+"Technician"
+],
+answer: 1
+},
+
+{
+question: "Which of the following is a programming language?",
+options: [
+"HTML",
+"HTTP",
+"FTP",
+"SMTP"
+],
+answer: 0
+},
+
+{
+question: "Which translates source code to machine code?",
+options: [
+"Editor",
+"Compiler",
+"Monitor",
+"Keyboard"
+],
+answer: 1
+},
+
+{
+question: "What is the main purpose of programming?",
+options: [
+"To decorate computers",
+"To instruct computers to perform tasks",
+"To repair hardware",
+"To browse the internet"
+],
+answer: 1
+},
+
+{
+question: "Which device is used to input program instructions?",
+options: [
+"Keyboard",
+"Printer",
+"Monitor",
+"Speaker"
+],
+answer: 0
+},
+
+{
+question: "Programs are written using:",
+options: [
+"Programming languages",
+"Operating systems",
+"Antivirus software",
+"Internet browsers"
+],
+answer: 0
+},
+
+{
+question: "Which of the following is NOT a programming language?",
+options: [
+"Python",
+"Java",
+"Windows",
+"C++"
+],
+answer: 2
+},
+
+{
+question: "The process of finding and fixing errors in a program is called:",
+options: [
+"Compiling",
+"Debugging",
+"Executing",
+"Scanning"
+],
+answer: 1
+}
+
+],
+
+dbms: [],
+os: []
+
+};
